be26bbd93eb1593b458539cf93e1c57e3d911085
Abdur Rahim <abdur.rahim@cqumail.com>
2026-08-02 13:54:32 +1000

    Escape user input to prevent HTML injection in results display

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+// escape special characters so user input can't break the results markup
+function escapeHtml(str) {
+  return String(str)
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#39;");
+}
+
 // wait for the page to load
 document.addEventListener("DOMContentLoaded", function () {
 
@@ -89,13 +99,109 @@ document.addEventListener("DOMContentLoaded", function () {
     // if everything is valid, display the results
     if (isValid) {
 
-      var html = "<table class='results-table'>";
-      html += "<tr><th>Full Name</th><td>" + fullName.value + "</td></tr>";
-      html += "<tr><th>Sex</th><td>" + sexValue + "</td></tr>";
-      html += "<tr><th>Age Group</th><td>" + ageGroup.value + "</td></tr>";
-      html += "<tr><th>State</th><td>" + state.value + "</td></tr>";
-      html += "<tr><th>Email</th><td>" + email.value + "</td></tr>";
-      html += "<tr><th>Mobile Phone</th><td>" + phone.value + "</td></tr>";
+      // group 1: personal details
+      var html = "<h3>Personal Details</h3>";
+      html += "<table class='results-table'>";
+      html += "<tr><th>Full Name</th><td>" + escapeHtml(fullName.value) + "</td></tr>";
+      html += "<tr><th>Sex</th><td>" + escapeHtml(sexValue) + "</td></tr>";
+      html += "<tr><th>Age Group</th><td>" + escapeHtml(ageGroup.value) + "</td></tr>";
+      html += "<tr><th>State</th><td>" + escapeHtml(state.value) + "</td></tr>";
+      html += "<tr><th>Email</th><td>" + escapeHtml(email.value) + "</td></tr>";
+      html += "<tr><th>Mobile Phone</th><td>" + escapeHtml(phone.value) + "</td></tr>";
+      html += "</table>";
+
+      // group 2: digital habits and work
+      var q1 = document.getElementById("q1");
+
+      var platforms = document.getElementsByName("platforms");
+      var platformValues = [];
+      for (var p = 0; p < platforms.length; p++) {
+        if (platforms[p].checked) {
+          platformValues.push(platforms[p].value);
+        }
+      }
+
+      var workType = document.getElementsByName("workType");
+      var workTypeValue = "";
+      for (var w = 0; w < workType.length; w++) {
+        if (workType[w].checked) {
+          workTypeValue = workType[w].value;
+        }
+      }
+
+      var q4 = document.getElementById("q4");
+      var q7 = document.getElementById("q7");
+
+      html += "<h3>Digital Habits & Work</h3>";
+      html += "<table class='results-table'>";
+      html += "<tr><th>Social Media Hours/Day</th><td>" + escapeHtml(q1.value) + "</td></tr>";
+      html += "<tr><th>Platforms Used</th><td>" + escapeHtml(platformValues.join(", ")) + "</td></tr>";
+      html += "<tr><th>Work Arrangement</th><td>" + escapeHtml(workTypeValue) + "</td></tr>";
+      html += "<tr><th>Job Title</th><td>" + escapeHtml(q4.value) + "</td></tr>";
+      html += "<tr><th>Screen Break Frequency</th><td>" + escapeHtml(q7.value) + "</td></tr>";
+      html += "</table>";
+
+      // group 3: mental wellbeing
+      var moodEffect = document.getElementsByName("moodEffect");
+      var moodValue = "";
+      for (var m = 0; m < moodEffect.length; m++) {
+        if (moodEffect[m].checked) {
+          moodValue = moodEffect[m].value;
+        }
+      }
+
+      var symptoms = document.getElementsByName("symptoms");
+      var symptomValues = [];
+      for (var s = 0; s < symptoms.length; s++) {
+        if (symptoms[s].checked) {
+          symptomValues.push(symptoms[s].value);
+        }
+      }
+
+      var q9 = document.getElementById("q9");
+
+      var soughtSupport = document.getElementsByName("soughtSupport");
+      var supportValue = "";
+      for (var sp = 0; sp < soughtSupport.length; sp++) {
+        if (soughtSupport[sp].checked) {
+          supportValue = soughtSupport[sp].value;
+        }
+      }
+
+      var q11 = document.getElementById("q11");
+      var q12 = document.getElementById("q12");
+
+      html += "<h3>Mental Wellbeing</h3>";
+      html += "<table class='results-table'>";
+      html += "<tr><th>Social Media Mood Effect</th><td>" + escapeHtml(moodValue) + "</td></tr>";
+      html += "<tr><th>Stress Symptoms</th><td>" + escapeHtml(symptomValues.join(", ")) + "</td></tr>";
+      html += "<tr><th>Wellbeing Rating (1-10)</th><td>" + escapeHtml(q9.value) + "</td></tr>";
+      html += "<tr><th>Sought Professional Support</th><td>" + escapeHtml(supportValue) + "</td></tr>";
+      html += "<tr><th>Coping Strategies</th><td>" + escapeHtml(q11.value) + "</td></tr>";
+      html += "<tr><th>Additional Comments</th><td>" + escapeHtml(q12.value) + "</td></tr>";
+      html += "</table>";
+
+      // group 4: your experience (likert statements)
+      var likertNames = ["likert_connected", "likert_compare", "likert_remote", "likert_disconnect"];
+      var likertLabels = [
+        "Feels connected via social media",
+        "Feels pressure to compare online",
+        "Remote work improved balance",
+        "Hard to disconnect from work"
+      ];
+
+      html += "<h3>Your Experience</h3>";
+      html += "<table class='results-table'>";
+      for (var l = 0; l < likertNames.length; l++) {
+        var options = document.getElementsByName(likertNames[l]);
+        var chosen = "";
+        for (var o = 0; o < options.length; o++) {
+          if (options[o].checked) {
+            chosen = options[o].value;
+          }
+        }
+        html += "<tr><th>" + likertLabels[l] + "</th><td>" + escapeHtml(chosen) + "</td></tr>";
+      }
       html += "</table>";
 
       resultsContent.innerHTML = html;
